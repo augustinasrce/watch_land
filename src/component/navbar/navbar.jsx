6c409761a5c49d7425c9355758ba173df1439d1a
DominykasSueris <dominykassueris@gmail.com>
2022-06-07 22:07:41 +0100

    nav button state onClick added

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
 const Navbar = () => {
+  const [selected, setSelected] = useState();
+
   return (
     <nav className="navbar">
       <span className="logo">Logo</span>
-      <Link className="navlink" to="/aws">
+      <Link
+        className={selected === "aws" ? "navlink selected" : "navlink"}
+        to="/aws"
+        onClick={() => setSelected("aws")}
+      >
         AWS
       </Link>
-      <Link className="navlink" to="/azure">
+      <Link
+        className={selected === "azure" ? "navlink selected" : "navlink"}
+        to="/azure"
+        onClick={() => setSelected("azure")}
+      >
         Azure
       </Link>
-      <Link className="navlink" to="/google">
+      <Link
+        className={selected === "google" ? "navlink selected" : "navlink"}
+        to="/google"
+        onClick={() => setSelected("google")}
+      >
         Google Cloud
       </Link>
     </nav>
