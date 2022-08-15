7c7111e4419a69be37fe0baa783ce948a0a7ca7b
DominykasSueris <dominykassueris@gmail.com>
2022-08-15 23:02:02 +0100

    navabar colors update with the map

--- watchland/src/components/Navbar/Navbar.tsx
+++ watchland/src/components/Navbar/Navbar.tsx
@@ -1,49 +1,49 @@
-import React, { FC, useState } from 'react';
-import {Link } from 'react-router-dom'
-import { getProviderGroups } from '../../services/services';
-import { ProviderTypes } from '../../utils/enum'
-import './Navbar.scss';
+import { FC, useState } from "react";
+import { Link } from "react-router-dom";
+import { getProviderGroups } from "../../services/services";
+import "./Navbar.scss";
 
 interface Service {
-  provider: string
+  provider: string;
 }
 
-interface MyService extends Service  {
-  myservice : typeof getProviderGroups[]
+interface MyService extends Service {
+  myservice: typeof getProviderGroups[];
 }
-interface NavbarProps{
-  isAuthenticated:boolean
+interface NavbarProps {
+  isAuthenticated: boolean;
 }
-const Navbar: FC<NavbarProps > = ({isAuthenticated}) => {
+const Navbar: FC<NavbarProps> = ({ isAuthenticated }) => {
   const [selected, setSelected] = useState<String | null>(null);
 
   return (
     <nav className="navbar">
       <span className="logo">Logo</span>
-      { isAuthenticated ? ([
-        <Link
-        className={selected === "aws" ? "navlink selected" : "navlink"}
-        to="/aws"
-        onClick={() => setSelected("aws")}
-      >
-        AWS
-      </Link>,
-      <Link
-        className={selected === "azure" ? "navlink selected" : "navlink"}
-        to="/azure"
-        onClick={() => setSelected("azure")}
-      >
-        Azure
-      </Link>,
-      <Link
-        className={selected === "google" ? "navlink selected" : "navlink"}
-        to="/google"
-        onClick={() => setSelected("google")}
-      >
-        Google Cloud
-      </Link>
-      ]) : null}
-      
+      {isAuthenticated
+        ? [
+            <Link
+              className={selected === "aws" ? "navlink selected" : "navlink"}
+              to="/aws"
+              onClick={() => setSelected("aws")}
+            >
+              AWS
+            </Link>,
+            <Link
+              className={selected === "azure" ? "navlink selected" : "navlink"}
+              to="/azure"
+              onClick={() => setSelected("azure")}
+            >
+              Azure
+            </Link>,
+            <Link
+              className={selected === "google" ? "navlink selected" : "navlink"}
+              to="/google"
+              onClick={() => setSelected("google")}
+            >
+              Google Cloud
+            </Link>
+          ]
+        : null}
     </nav>
   );
 };
