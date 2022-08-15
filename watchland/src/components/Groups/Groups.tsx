c8ca33b1d85f8b15d31651ad6133957822f3b824
DominykasSueris <dominykassueris@gmail.com>
2022-08-15 23:21:03 +0100

    Init commit for groups compoment

--- watchland/src/components/Groups/Groups.tsx
+++ watchland/src/components/Groups/Groups.tsx
@@ -1,16 +1,44 @@
-import React, { FC, useState } from 'react';
-import './Groups.scss';
+import React, { useState } from "react";
+import { IProviderGroup } from "../../utils/interfaces";
+import { GoTriangleRight } from "react-icons/go";
+import { GoTriangleDown } from "react-icons/go";
 
-interface GroupsProps {}
+interface GroupProps {
+  group: IProviderGroup;
+}
 
-const Groups: FC<GroupsProps> = () => {
-  const [groups, setGroup] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const pageSize = 10;
+const Group = ({ group }: GroupProps) => {
+  const [expanded, setExpanded] = useState<boolean>(false);
 
+  const getDetails = () => {
+    if (!expanded) return null;
+    return (
+      <tr className="info">
+        <td>{group.stream}</td>
+      </tr>
+    );
+  };
 
   return (
-    <div></div>
-  )
-}
-export default Groups;
+    <React.Fragment>
+      <tr className={`group ${expanded ? "expanded" : ""}`} key={group.id}>
+        <td className="group-id">{group.id}</td>
+        <td className="group-stream"> {group.stream}</td>
+        {!expanded ? (
+          <td className="group-lastEvent">
+            <GoTriangleRight className="arrow-right" onClick={() => setExpanded(!expanded)} />
+            {group.lastEvent}
+          </td>
+        ) : (
+          <td className="group-lastEvent">
+            <GoTriangleDown className="arrow-down" onClick={() => setExpanded(!expanded)} />
+            {group.lastEvent}
+          </td>
+        )}
+      </tr>
+      {getDetails()}
+    </React.Fragment>
+  );
+};
+
+export default Group;
