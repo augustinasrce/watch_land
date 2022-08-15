ee637ab21d74dafd624196f120fe7d1ada4aac02
DominykasSueris <dominykassueris@gmail.com>
2022-08-15 23:34:19 +0100

    Default authentication added to the main component

--- watchland/src/App.tsx
+++ watchland/src/App.tsx
@@ -1,29 +1,28 @@
-import './App.scss';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from './components/Navbar/Navbar';
-import Provider from './components/Provider/Provider';
-import { ProviderTypes } from './utils/enum';
+import Navbar from "./components/Navbar/Navbar";
+import Provider from "./components/Provider/Provider";
+import { ProviderTypes } from "./utils/enum";
+import "./css/style.css";
 
-const isAuthenticated = ()=>{
+const isAuthenticated = () => {
   return true;
-}
+};
+
 function App() {
   return (
     <Router>
-
-    <div className="App">
-    <Navbar isAuthenticated={ isAuthenticated() }/>
-    <main className="container">
-      <Routes>
-        <Route path="/aws" element={<Provider type={ProviderTypes.AWS} />}></Route>
-        <Route path="/azure" element={<Provider type={ProviderTypes.AZURE} />}></Route>
-        <Route path="/google" element={<Provider type={ProviderTypes.GOOGLE} />}></Route>
-      </Routes>
-    </main>
-  </div>
-      </Router>
+      <div className="App">
+        <Navbar isAuthenticated={isAuthenticated()} />
+        <main className="container">
+          <Routes>
+            <Route path="/aws" element={<Provider type={ProviderTypes.AWS} />}></Route>
+            <Route path="/azure" element={<Provider type={ProviderTypes.AZURE} />}></Route>
+            <Route path="/google" element={<Provider type={ProviderTypes.GOOGLE} />}></Route>
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
