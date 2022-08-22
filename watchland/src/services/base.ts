961f2f39f8159e1ef1da88a0072e67914995fc4f
DominykasSueris <dominykassueris@gmail.com>
2022-08-22 21:41:37 +0100

    Deleted unnecessary  constructor

--- watchland/src/services/base.ts
+++ watchland/src/services/base.ts
@@ -1,16 +1,13 @@
 import { IApiProvider, IProviderGroup } from "../utils/interfaces";
 
-export class BaseApiProvider implements IApiProvider{
-    constructor(){
-        
-    }
-    async apiCall(url: string): Promise<any> {
- 
-    }
-    async getGroups(): Promise<IProviderGroup[]|[]>{
-        return []
-    }
-    async getStream(streamId: string): Promise<void> {
-     
-    }      
+export class BaseApiProvider implements IApiProvider {
+  async apiCall(url: string): Promise<any> {}
+  async getGroups(): Promise<IProviderGroup[] | []> {
+    return [];
+  }
+
+  async getGroupById(id: number): Promise<IProviderGroup | undefined> {
+    return undefined;
+  }
+  async getStream(streamId: string): Promise<void> {}
 }
