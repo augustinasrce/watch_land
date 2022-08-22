7ab6e3d6936a9e1ef623900600d40ac87cb10ca9
DominykasSueris <dominykassueris@gmail.com>
2022-08-22 22:03:12 +0100

    Added getGroupId function

--- watchland/src/services/services.ts
+++ watchland/src/services/services.ts
@@ -1,14 +1,22 @@
 import { ProviderTypes } from "../utils/enum";
+import { IProviderGroup } from "../utils/interfaces";
 import { AwsApiProvider } from "./aws";
 import { AzureApiProvider } from "./azure";
 import { GoogleApiProvider } from "./google";
 
-export const getProviderGroups = (type:ProviderTypes)=>{
-    const providers = {
-        "aws":AwsApiProvider,
-        "azure":AzureApiProvider,
-        "google":GoogleApiProvider
-    }
-    const provider = new providers[type]()
-    return provider.getGroups()
-}
+const providers = {
+  aws: AwsApiProvider,
+  azure: AzureApiProvider,
+  google: GoogleApiProvider
+};
+
+export const getProviderGroups = (type: ProviderTypes) => {
+  const provider = new providers[type]();
+  return provider.getGroups();
+};
+
+export const getGroupById = (id: number, type: ProviderTypes) => {
+  const provider = new providers[type]();
+  console.log(provider);
+  return provider.getGroupById(id);
+};
