68615611d825742bd1fda5866c9d1f3e90409698
DominykasSueris <dominykassueris@gmail.com>
2022-08-31 21:07:24 +0100

    Aws logs table component added

--- watchland/src/components/Providers/AWS/AwsLogs.tsx
+++ watchland/src/components/Providers/AWS/AwsLogs.tsx
@@ -1,4 +1,12 @@
+import { useState, useEffect } from "react";
 import { IProviderGroup } from "../../../utils/interfaces";
+import { IAwsLogs } from "./spec";
+import { ITableCell } from "../../spec";
+import { getProviderGroups } from "../../../services/services";
+import { ProviderTypes } from "../../../utils/enum";
+import { tableCellObject } from "../../../utils/objects";
+import { slugifyString } from "../../../utils/strings";
+import Table from "../../Table/Table";
 
 interface IAWSProps {
   groups: IProviderGroup[];
@@ -6,7 +14,35 @@ interface IAWSProps {
 }
 
 const AwsLogs = () => {
-    /**
+  const [logs, setLogs] = useState<IAwsLogs[]>([]);
+  const [body, setBody] = useState<ITableCell[][]>([]);
+  const loadLogs = async () => {
+    const logs: any[] = await getProviderGroups(ProviderTypes.AWS);
+    setLogs(logs);
+  };
+
+  useEffect(() => {
+    loadLogs();
+  });
+
+  useEffect(() => {
+    const bodyCells = () => {
+      return [
+        ...logs?.map((log: IAwsLogs) => {
+          const logName = tableCellObject(
+            log.logStreamName,
+            true,
+            slugifyString(log.logStreamName)
+          );
+          const logMessage = tableCellObject(log.message, false, "");
+          const logTimeStamp = tableCellObject(log.timestamp.toString(), false, "");
+          return [logName, logMessage, logTimeStamp];
+        })
+      ];
+    };
+    setBody(bodyCells);
+  }, [logs]);
+  /**
      *      logStreamName: string,
             timestamp: number
             message: string
@@ -15,9 +51,7 @@ const AwsLogs = () => {
 
             Display all inside the table component
      */
-    return (
-        <div>AWS Logs</div>
-    );
+  return <Table headers={["Log stream name", "Log message"]} body={body} />;
 };
 
 export default AwsLogs;
