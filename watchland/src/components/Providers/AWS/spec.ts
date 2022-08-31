4d0f419a0835d0e22040ce61a7cccdbd156115ca
DominykasSueris <dominykassueris@gmail.com>
2022-08-31 21:06:53 +0100

    Added IAwsLogs interface

--- watchland/src/components/Providers/AWS/spec.ts
+++ watchland/src/components/Providers/AWS/spec.ts
@@ -1,6 +1,14 @@
-export interface IAwsLogGroups{
-    logGroupName: string
-    creationTime: string
-    metricFilterCount: number
-    arn: string
+export interface IAwsLogGroups {
+  logGroupName: string;
+  creationTime: string;
+  metricFilterCount: number;
+  arn: string;
+}
+
+export interface IAwsLogs {
+  logStreamName: string;
+  timestamp: number;
+  message: string;
+  ingestionTime: number;
+  eventId: string;
 }
