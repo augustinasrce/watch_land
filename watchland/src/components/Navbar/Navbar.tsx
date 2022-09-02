ceba4233709205f4e4c92e8d5354fbf0888336eb
konstantin <konstantin@zappar.com>
2022-09-02 17:31:22 +0100

    Fixed bootstrap navbar

--- watchland/src/components/Navbar/Navbar.tsx
+++ watchland/src/components/Navbar/Navbar.tsx
@@ -9,29 +9,38 @@ const Navbar: FC<NavbarProps> = ({ isAuthenticated }) => {
   const [selected, setSelected] = useState<String | undefined>(undefined);
 
   return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="logo">Logo</div>
-      <Link
-        className={selected === "aws" ? "navlink selected" : "navlink"}
-        to="/aws"
-        onClick={() => setSelected("aws")}
-      >
-        AWS
-      </Link>
-      <Link
-        className={selected === "azure" ? "navlink selected" : "navlink"}
-        to="/azure"
-        onClick={() => setSelected("azure")}
-      >
-        Azure
-      </Link>
-      <Link
-        className={selected === "google" ? "navlink selected" : "navlink"}
-        to="/google"
-        onClick={() => setSelected("google")}
-      >
-        Google Cloud
-      </Link>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">WatchLand 1.0</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">     
+            <li className="nav-item">
+              <Link
+                className={selected === "aws" ? "nav-link active" : "nav-link"}
+                to="/aws/groups/"
+                onClick={() => setSelected("aws")}
+              >AWS</Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={selected === "azure" ? "nav-link active" : "nav-link"}
+                to="/azure/groups/"
+                onClick={() => setSelected("azure")}
+              >Azure</Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={selected === "google" ? "nav-link active" : "nav-link"}
+                to="/google/groups/"
+                onClick={() => setSelected("google")}
+              >Google Cloud</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
