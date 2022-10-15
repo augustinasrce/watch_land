3f75d110e2d2fab72664a7a5b9fa2f8eeed184e3
DominykasSueris <dominykassueris@gmail.com>
2022-10-15 20:26:05 +0100

    utils component created for session_methods_key

---
+++ src/utils.ts
@@ -0,0 +1,24 @@
+import { IProfile } from "./redux/specs/authSpecs";
+
+export class AuthSessions {
+  static session_methods_key = "auth_methods";
+
+  static getMethods() {
+    let sessionMethods = window.sessionStorage.getItem(this.session_methods_key);
+    if (!sessionMethods) return [];
+    const methods = [...JSON.parse(sessionMethods)];
+    return methods;
+  }
+
+  static setMethods(methods: IProfile[]) {
+    const sessionStorage = window.sessionStorage;
+    const methodsString = JSON.stringify(methods);
+    sessionStorage.setItem(this.session_methods_key, methodsString);
+  }
+
+  static updateMethods(method: IProfile) {
+    const sessionMethods = this.getMethods();
+    sessionMethods.push(method);
+    this.setMethods(sessionMethods);
+  }
+}
