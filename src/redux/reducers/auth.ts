675355b0715a05340c88a02fd1cf467710fa4a38
DominykasSueris <dominykassueris@gmail.com>
2022-10-15 20:37:00 +0100

    changes in auth reducer

--- src/redux/reducers/auth.ts
+++ src/redux/reducers/auth.ts
@@ -1,4 +1,10 @@
-import { IAuthAction, IAuthState } from "../specs/authSpecs";
+import {
+  IAuthAction,
+  IAuthActionDisconnect,
+  IAuthConnectionAction,
+  IAuthState,
+  IProfile
+} from "../specs/authSpecs";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const initialState: IAuthState = {
@@ -10,6 +16,9 @@ export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
+    updateConnections: (state, action: PayloadAction<IAuthConnectionAction>) => {
+      state.methods = action.payload.data;
+    },
     cloudConnect: (state, action: PayloadAction<IAuthAction>) => {
       state.current = {
         id: action.payload.data.id,
@@ -17,22 +26,15 @@ export const authSlice = createSlice({
       };
       state.methods.push(action.payload.data);
     },
-    cloudDisconnect: (state, action) => {
-      // Abort if there is no current connection to disconnect
-      if (state.current == null) return;
-
-      const id = state.current.id;
-      const provider = state.current.provider;
-      state.methods = state.methods.filter(loginMethod => loginMethod.id !== id);
-
-      if (state.methods.length > 0) {
-        state.current = { id: state.methods[0].id, provider: provider };
-      } else {
-        state.current = null;
-      }
+    cloudDisconnect: (state, action: PayloadAction<IAuthActionDisconnect>) => {
+      const currentMethods = state.methods;
+      const newMethods = currentMethods.filter(
+        (method: IProfile) => method.id === action.payload.id
+      );
+      state.methods = newMethods;
     }
   }
 });
 
-export const { cloudConnect, cloudDisconnect } = authSlice.actions;
+export const { cloudConnect, cloudDisconnect, updateConnections } = authSlice.actions;
 export default authSlice.reducer;
