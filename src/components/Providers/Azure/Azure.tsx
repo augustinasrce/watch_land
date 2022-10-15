24c0444039107332a9b2e73b7e8239b0f0c2e782
DominykasSueris <dominykassueris@gmail.com>
2022-10-15 20:29:43 +0100

    import changes in Azure and gCloud components

--- src/components/Providers/Azure/Azure.tsx
+++ src/components/Providers/Azure/Azure.tsx
@@ -2,14 +2,14 @@ import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import { AuthTarget } from "../../../redux/specs/authSpecs";
 import { RootState } from "../../../redux/store";
-import Login from "../../Navbar/Login";
+import Login from "../../Auth/Login";
 
 const Azure = () => {
   const isAuth = useSelector((state: RootState) => {
     return state.auth.current?.provider === AuthTarget.Azure;
   });
 
-  return isAuth ? <Outlet></Outlet> : <Login isAuth={isAuth} />;
+  return isAuth ? <Outlet></Outlet> : <Login isAuth={isAuth} provider={AuthTarget.Azure} />;
 };
 
 export default Azure;
--- src/components/Providers/gCloud/GoogleCloud.tsx
+++ src/components/Providers/gCloud/GoogleCloud.tsx
@@ -2,14 +2,14 @@ import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import { AuthTarget } from "../../../redux/specs/authSpecs";
 import { RootState } from "../../../redux/store";
-import Login from "../../Navbar/Login";
+import Login from "../../Auth/Login";
 
 const GoogleCloud = () => {
   const isAuth = useSelector((state: RootState) => {
-    return state.auth.current?.provider === AuthTarget.Azure;
+    return state.auth.current?.provider === AuthTarget.gCloud;
   });
 
-  return isAuth ? <Outlet></Outlet> : <Login isAuth={isAuth} />;
+  return isAuth ? <Outlet></Outlet> : <Login isAuth={isAuth} provider={AuthTarget.gCloud} />;
 };
 
 export default GoogleCloud;
