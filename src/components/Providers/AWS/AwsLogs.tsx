ab5f15c70d61832f9f36c7feb00c65edde27c2fb
DominykasSueris <dominykassueris@gmail.com>
2022-11-05 20:47:56 +0000

    AwsLogs connection and error alert added

--- src/components/Providers/AWS/AwsLogs.tsx
+++ src/components/Providers/AWS/AwsLogs.tsx
@@ -1,23 +1,26 @@
 import { useState, useEffect } from "react";
 import { ITableCell } from "../../spec";
-import { ProviderTypes } from "../../../utils/enum";
 import { tableCellObject } from "../../../utils/objects";
-import { getLogs } from "../../../services/services";
 import { IAwsLogs } from "../../../services/aws/spec";
 import { useQuery } from "../../../utils/hooks";
 import Table from "../../Table/Table";
 import BackButton from "../../BackButton/BackButton";
+import { CloudWatch } from "../../../services/aws/aws";
+import ErrorAlert from "../../Alert/ErrorAlert";
 
 const AwsLogs = () => {
   const groupName = useQuery().get("group") || "";
-  const stream = useQuery().get("stream") || "";
   const [logs, setLogs] = useState<IAwsLogs[]>([]);
   const [body, setBody] = useState<ITableCell[][]>([]);
+  const [error, setError] = useState<boolean>(false);
   const loadLogs = async () => {
-    const logs: any[] = await getLogs(groupName, [stream], ProviderTypes.AWS);
-    setLogs(logs);
+    const logGroups = [{ group: groupName }];
+    CloudWatch.logs(logGroups)
+      .observe(data => {
+        setLogs(data);
+      })
+      .catch(() => setError(true));
   };
-
   useEffect(() => {
     loadLogs();
   }, []);
@@ -41,6 +44,7 @@ const AwsLogs = () => {
   }, [logs]);
   return (
     <>
+      {error ? <ErrorAlert /> : null}
       <BackButton />
       <Table headers={["Timestamp", "Message", "Log stream name"]} body={body} />
     </>
