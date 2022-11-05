68a62a097f7769b0d3d220f17e47e753071e51ca
DominykasSueris <dominykassueris@gmail.com>
2022-11-05 20:44:11 +0000

    Error alert component created

---
+++ src/components/Alert/ErrorAlert.tsx
@@ -0,0 +1,12 @@
+const ErrorAlert = () => {
+  return (
+    <div
+      className="alert alert-danger col-sm-12 col-md-6 offset-md-3 text-center mt-4"
+      role="alert"
+    >
+      Something went wrong!
+    </div>
+  );
+};
+
+export default ErrorAlert;
