b8c385cfb5b01ff126d25bbed3837c8982544c29
DominykasSueris <dominykassueris@gmail.com>
2022-11-05 20:46:31 +0000

    Error alert to awsGroups comp added

--- src/components/Providers/AWS/AwsGroups.tsx
+++ src/components/Providers/AWS/AwsGroups.tsx
@@ -5,14 +5,18 @@ import { ITableCell } from "../../spec";
 import Table from "../../Table/Table";
 import BackButton from "../../BackButton/BackButton";
 import { CloudWatch } from "../../../services/aws/aws";
+import ErrorAlert from "../../Alert/ErrorAlert";
 
 const AwsGroups = () => {
   const [groups, setGroups] = useState<IAwsLogGroups[]>([]);
   const [body, setBody] = useState<ITableCell[][]>([]);
+  const [error, setError] = useState<boolean>(false);
   const loadGroups = async () => {
-    CloudWatch.groups().observe(data => {
-      setGroups(data);
-    });
+    CloudWatch.groups()
+      .observe(data => {
+        setGroups(data);
+      })
+      .catch(() => setError(true));
   };
 
   useEffect(() => {
@@ -38,6 +42,7 @@ const AwsGroups = () => {
 
   return (
     <>
+      {error ? <ErrorAlert /> : null}
       <BackButton />
       <Table headers={["Log group", "Creation time"]} body={body} />
     </>
