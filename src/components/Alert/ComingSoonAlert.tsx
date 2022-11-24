0a3d7652e636415b7a98eb8ed8d684a7a2a00ce7
DominykasSueris <dominykassueris@gmail.com>
2022-11-24 22:32:07 +0000

    ComingSoon component created

---
+++ src/components/Alert/ComingSoonAlert.tsx
@@ -0,0 +1,12 @@
+const ComingSoonAlert = () => {
+  return (
+    <div
+      className="alert alert-warning col-sm-12 col-md-6 offset-md-3 text-center mt-4"
+      role="alert"
+    >
+      Coming Soon!!!
+    </div>
+  );
+};
+
+export default ComingSoonAlert;
