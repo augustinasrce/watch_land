d8e73453d01c2a9f143db276ba1676bd883b5129
DominykasSueris <dominykassueris@gmail.com>
2023-01-10 20:23:21 +0000

    date error bug fix

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,8 +1,10 @@
 export const dateToDateTimeStr = (timeStamp: number) => {
   const dateObj = new Date(timeStamp);
   const year = dateObj.getFullYear();
-  const month = dateObj.getMonth() + 1;
-  const day = dateObj.getDate();
+  const monthNum = dateObj.getMonth() + 1;
+  const month = monthNum < 10 ? `0${monthNum}` : monthNum;
+  const dayNum = dateObj.getDate();
+  const day = dayNum < 10 ? `0${dayNum}` : dayNum;
   const hour = (dateObj.getHours() < 10 ? "0" : "") + dateObj.getHours();
   const minute = (dateObj.getMinutes() < 10 ? "0" : "") + dateObj.getMinutes();
   return `${year}-${month}-${day}T${hour}:${minute}`;
