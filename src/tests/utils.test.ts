157b5f69510fe9190cc873fdae44e527fd490e0a
Dominykas Sueris <dominykassueris@gmail.com>
2023-01-18 21:21:52 +0000

    utilities tests added

---
+++ src/tests/utils.test.ts
@@ -0,0 +1,54 @@
+import { AuthSessions } from "../utils";
+import { AuthRegion, AuthTarget } from "../redux/specs/authSpecs";
+import { WLDevProfiles } from "../services/specs";
+
+beforeEach(() => {
+  window.sessionStorage.clear();
+});
+
+test("test if getmethods returns empty array, when session is empty", () => {
+  expect(AuthSessions.getMethods()).toStrictEqual([]);
+});
+test("test if getMethod are parsed to string", () => {
+  window.sessionStorage.setItem("auth_methods", '[{"master_key": "cat"}]');
+  expect(AuthSessions.getMethods()).toStrictEqual([{ master_key: "cat" }]);
+});
+test("test if getMethod are parsed to string if more than one value", () => {
+  window.sessionStorage.setItem(
+    "auth_methods",
+    '[{"master_key": "cat", "late_convenience": "dog" }]'
+  );
+  expect(AuthSessions.getMethods()).toStrictEqual([{ master_key: "cat", late_convenience: "dog" }]);
+});
+test("test if setMethods updates sessionStorage ", () => {
+  const method = {
+    id: "1",
+    type: WLDevProfiles.Programmatic,
+    provider: AuthTarget.AWS,
+    region: AuthRegion.Africa
+  };
+  AuthSessions.setMethods([method]);
+  const item = window.sessionStorage.getItem("auth_methods");
+  const expectedResults = JSON.stringify([method]);
+
+  expect(item).toStrictEqual(expectedResults);
+});
+test("test if updateMethods updates sessionStorage", () => {
+  const method1 = {
+    id: "1",
+    type: WLDevProfiles.Programmatic,
+    provider: AuthTarget.AWS,
+    region: AuthRegion.Africa
+  };
+  const method2 = {
+    id: "2",
+    type: WLDevProfiles.Programmatic,
+    provider: AuthTarget.AWS,
+    region: AuthRegion.Africa
+  };
+  AuthSessions.setMethods([method1]);
+  AuthSessions.updateMethods(method2);
+  expect(window.sessionStorage.getItem("auth_methods")).toStrictEqual(
+    JSON.stringify([method1, method2])
+  );
+});
