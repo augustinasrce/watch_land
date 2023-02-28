b8f333b884aea701c13c3af032f326dafcb1ac89
Dominykas Sueris <dominykassueris@gmail.com>
2023-02-28 22:12:44 +0000

    AwsLogs openable row added Ref #26

--- src/components/Providers/AWS/AwsLogsRow.tsx
+++ src/components/Providers/AWS/AwsLogsRow.tsx
@@ -1,4 +1,5 @@
 import { IAwsLogs } from "../../../services/aws/spec";
+import { useState } from "react";
 import { timestampToDate } from "../../../utils/dates";
 
 interface IAwsLogsRow {
@@ -6,13 +7,25 @@ interface IAwsLogsRow {
 }
 
 const AwsLogsRow = ({ log }: IAwsLogsRow) => {
+  const [open, setOpen] = useState<Boolean>(false);
+
+  const toggle = () => {
+    setOpen(current => !current);
+  };
   return (
     <>
       {log ? (
-        <tr>
+        <tr onClick={toggle}>
           <td>{log.logStreamName}</td>
           <td>{log.message}</td>
           <td> {timestampToDate(log.timestamp)}</td>
+        </tr>
+      ) : null}
+      {open ? (
+        <tr onClick={toggle}>
+          <td style={{ borderTop: "none" }} colSpan={3}>
+            {log?.logStreamName}
+          </td>
         </tr>
       ) : null}
     </>
